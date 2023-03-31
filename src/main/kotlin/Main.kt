1d71ca256d696fc81984b8e743eb0618d014ffd6
JuanLuisCoutino <123596662+JuanLuisCoutino@users.noreply.github.com>
2023-03-30 20:01:37 -0600

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    
+    var moto = Motocicletas()
+    moto.llantas()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var auto = Auto()
+    auto.llantas()
+
+    var camion = CamionTruck()
+    camion.llantas()
 }
